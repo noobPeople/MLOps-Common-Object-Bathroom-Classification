9b724e71e5252bf207a663472f8b3d10a3f3cc6a
Zakky Ghufron <zakky.okky@gmail.com>
2026-04-16 00:04:01 +0700

    initial structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='first Machine Learning projects for classifying common object bathroom by incorporating MLOps methods and tools to build production-grade model',
+    author='zakky ghufron',
+    license='',
+)
